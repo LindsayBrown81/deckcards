6e90cf001fae6156919366f9f212d8e97b7ffaad
LindsayBrown81 <LindsayBrown81@users.noreply.github.com>
2017-03-02 23:51:59 -0500

    start coding project to build deck of cards using JavaScript and CSS without using HTML template

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # deckcards
+a coding challenge to create a deck of cards in JavaScript and CSS. My first approach used an HTML template. My second approach will use strictly JavaScript and CSS. I'm opting to use Vanilla JavaScript.
---
+++ cards.js
@@ -0,0 +1,141 @@
+// Vanilla JavaScript. No templating framework is used.
+window.onload= function cardMaker () {
+
+	"use strict";
+
+	var cardJson = {},
+		sym = "";
+		
+	// Hardcoded JSON
+	cardJson = 
+	  {
+	  	"syms": [ "♠", "♥", "♣", "♦" ],
+	  	"chars": [ "A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+	  };
+
+	// No longer allowed to use a template
+	// theTemplate = document.getElementById("the-template"); 	
+  
+  	// I'm forming a plan to dynamically generate/declare and initialize 
+  	// 1) a card list which will be the only element appended onto the document
+  	//    root (only one is allowed, correct?). So, cardObjList would be the parent. 
+  	// 2) a card object which I'll name cardObj, and 
+  	// 3) card parts such as suit div and rank div. I think I will need to create text 
+  	//    nodes first, then use appendChild(sym or char 
+  	//    items from json), which will be similar to original approach starting on line 38
+  	
+  	var cardObjList = document.createElement("ul");
+	// cardObjList = contentDocument.documentElement;
+ 	// document.appendChild(cardObjList);
+ 	
+ 	// Create an anchor span on which all cards will be appended at the bottom of the loop.
+	// After the loop terminates, then append this anchor to the actual DOM.   
+	var anchor = document.createElement("span");
+
+	// Declare and initialize card parts
+	var cardObj = document.createElement("div");
+		topLeft = document.createElement("div");
+	 	bottomRight = document.createElement("div");
+	 	suit = document.createElement("div");
+	 	rank = document.createElement("div");
+	 	theCenter = document.createElement("div");
+  
+		anchor.className = "flex-container";
+		cardObj.className = "card flex-container";
+		topLeft.className = "top-left";
+		bottomRight.className = "bottom-right upsidedown";
+		suit.className = "suit";
+		rank.className = "rank";
+		theCenter.className = "the-center flex-container";
+  
+	// Loop through the first array in cardJson and ...
+	for (let sym of cardJson.syms) { 
+        
+      let char = "";
+
+		// ...for each item in first array, loop through the second array	
+	  	for (let char of cardJson.chars) {
+        // cardObj = {},
+	  		let suitsArr = [],
+			    ranksArr = [],	
+			    centerArr = [],		
+			    suitDiv = {},
+			    rankDiv = {},
+			    centerDiv = {};
+
+		  	// No longer allowed to use template: Create deep clone of the template with true
+			// cardObj = theTemplate.cloneNode(true);
+
+			// Is my cardObj a DOM el yet if it has not yet been appended to the DOM?
+			// If not, I cannot use setAttribute 
+			cardObj.setAttribute("id", sym + char );
+           
+      		// Create a unique reference for all cards/get their SUIT class divs
+			suitsArr = cardObj.querySelectorAll("#" + sym + char + " .suit");
+
+			// Add sym json values to suit class divs
+			for (let suitDiv of suitsArr) {
+				suitDiv.appendChild(document.createTextNode(sym) );
+			}
+			
+			// Create a unique reference for all cards/get their RANK class divs
+			ranksArr = cardObj.querySelectorAll("#" + sym + char + " .rank");
+			
+			// Add char json values to rank class divs
+			for (let rankDiv of ranksArr) {
+				rankDiv.appendChild(document.createTextNode(char) ); 
+			}
+		   
+			// Create a unique reference for all cards/get their THE-CENTER class divs
+			centerArr = cardObj.querySelectorAll("#" + sym + char + " .the-center");
+			
+			// Loop through the centers of all cards, copy existing div and append to center
+			for (let centerDiv of centerArr) {
+
+				if (typeof char === "string") {
+					centerDiv.classList.add("facecard");
+					 
+					// Access one existing suit from card, clone, append to center div
+					var rankCopy = ranksArr[0].cloneNode(true);
+					centerDiv.appendChild(rankCopy);					
+					
+				} else {
+					centerDiv.classList.add("numcard");
+
+					// Access one existing suit from card, clone on each iteration. 
+					// Number of iterations will match value of char. Append to center div
+					var times = char;
+
+					for (var i=0; i<times; i++) {
+						var suitCopy = suitsArr[0].cloneNode(true);
+						centerDiv.appendChild(suitCopy);
+					}
+				}
+
+			}
+
+			// If the card is a heart or diamond suit
+			if (sym === "♥" || sym === "♦") {
+	       		// Get card, add class red
+	       		cardObj.classList.add("red");
+	   		} 
+
+	   		anchor.appendChild(cardObj);
+
+	   		// cardObjList.push(cardObj);
+				
+			// Add all cardObjs to anchor span, to be rendered when loop has terminated
+			cardObjList.appendChild(anchor);
+		}
+				  
+	}	
+
+	// Finally, append anchor span on to DOM after the loop has terminated 
+	// document.getElementById("card-obj-list").appendChild(anchor); 
+  // document.appendChild(cardObjList);
+
+  // cardObjList.appendChild("anchor");
+  // document.appendChild(cardObjList);
+	
+}();
+
