413a8a95837745f713b8c154cc6574f81a77bffd
LindsayBrown81 <LindsayBrown81@users.noreply.github.com>
2017-03-06 01:33:18 -0500

    a new loop mimicks card template from original version. 52 cardObj divs exist but not yet appended to element I appended DocumentFragment to. Not yet looping through all 52 ids.

--- cards.js
+++ cards.js
@@ -3,16 +3,6 @@ window.onload= function cardMaker () {
 
 	"use strict";
 
-	var cardJson = {},
-		sym = "";
-		
-	// Hardcoded JSON
-	cardJson = 
-	  {
-	  	"syms": [ "♠", "♥", "♣", "♦" ],
-	  	"chars": [ "A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-	  };
-
 	// No longer allowed to use a template
 	// theTemplate = document.getElementById("the-template"); 	
   
@@ -23,53 +13,53 @@ window.onload= function cardMaker () {
   	// 3) card parts such as suit div and rank div. I think I will need to create text 
   	//    nodes first, then use appendChild(sym or char 
   	//    items from json), which will be similar to original approach starting on line 38
-  	
-  	var cardObjList = document.createElement("ul");
-	// cardObjList = contentDocument.documentElement;
- 	// document.appendChild(cardObjList);
- 	document.body.appendChild(cardObjList);
  	
- 	// Create an anchor span on which all cards will be appended at the bottom of the loop.
-	// After the loop terminates, then append this anchor to the actual DOM. 
-	var anchor = document.createElement("span");
-
-	// var cardObj = document.createElement("div");
-
-	// Make card divs but don't add text yet
-	function makeDiv(){
-		var div = document.createElement("div");
-		// div.appendChild(document.createTextNode(text));
-		return div;
-	}
-	
-	var divs = [
-		makeDiv("cardObj"), // ERROR: cardObj is not defined STOP POINT
-		makeDiv("topLeft"),
-	 	makeDiv("bottomRight"),
-	 	makeDiv("suit"),
-	 	makeDiv("rank"),
-	 	makeDiv("theCenter")
- 	];
-
- 	var docFragment = document.createDocumentFragment();
+ 	// DocumentFragment must be the overarching parent element of appended children
+ 	// and the only el allowed to be appended to the document root. Anchor likely no longer needed.
+ 	// I'm going to use the image tag as the element to append my docFragment UL to
+ 	// because I like where it is positioned on the page. 
+ 	// I'll call appendChild method on element after the make template loop terminates.
+ 		// WRONG: docFragment is a UL which will have 52 child cardObj LIs appended to it before the outer loop terminates.
+ 	// docFragment is a UL which will have one child cardObj LI appended to it immediately before I append docFragment to 
+ 	// the pre-existing element.
+ 	var element  = document.getElementsByTagName("img")[0];
+	// The fragment vanishes upon getting appended, leaving only its children (child LI?) appended to element.
+ 	var docFragment = document.createDocumentFragment("ul");
  	
- 	for(var i=0; i<divs.length; i++) {
+	// Make template with a loop that creates child divs for one cardObj but don't add text yet
+	var divs = ["card flex-container", "top-left", "bottom-right upsidedown", "suit", "rank", "the-center flex-container"];
+
+ 	for (var i=0; i<divs.length; i++) {
  		console.log(divs[i]);
- 		docFragment.appendChild(divs[i]); // not yet appended to document root on DOM
+ 		
+  		var div = document.createElement("div");
+  		// Assign classes to divs for referencing and for CSS layout
+  		div.className = (divs[i]);
+		// for (let card of cardArr) {
+				// card.appendChild(document.createElement("div") );
+		// 	}
+ 		// cardObj.appendChild(div); // not yet appended to document root on DOM
+ 		docFragment.appendChild(div);
 	}
-	// Append all divs to anchor for card
-	// anchor.appendChild(docFragment);
-	cardObjList.appendChild(docFragment);
-  	
-  	// Assign classes to divs for layout and painting	
-	anchor.className = "flex-container";
-	cardObj.className = "card flex-container";
-	topLeft.className = "top-left";
-	bottomRight.className = "bottom-right upsidedown";
-	suit.className = "suit";
-	rank.className = "rank";
-	theCenter.className = "the-center flex-container";
 
+	element.appendChild(docFragment);
+	
+	// Make a card object and append child divs to it. This will serve as a card template div that can be cloned.
+	var cardObj = {};
+	cardObj = element.querySelector(" .card");
+	console.log("cardObj: ", cardObj);
+
+	
+	var cardJson = {},
+		sym = "";
+		
+	// Hardcoded JSON for storing card data
+	cardJson = 
+	  {
+	  	"syms": [ "♠", "♥", "♣", "♦" ],
+	  	"chars": [ "A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+	  };
+ 	
 	// Add text nodes to divs  
 	// Loop through the first array in cardJson and ...
 	for (let sym of cardJson.syms) { 
@@ -78,7 +68,7 @@ window.onload= function cardMaker () {
 
 		// ...for each item in first array, loop through the second array	
 	  	for (let char of cardJson.chars) {
-        // cardObj = {},
+        // 
 	  		let suitsArr = [],
 			    ranksArr = [],	
 			    centerArr = [],		
@@ -88,10 +78,10 @@ window.onload= function cardMaker () {
 
 		  	// No longer allowed to use template: Create deep clone of the template with true
 			// cardObj = theTemplate.cloneNode(true);
+			
 
-			// Is my cardObj a DOM el yet if it has not yet been appended to the DOM?
-			// If not, I cannot use setAttribute 
 			cardObj.setAttribute("id", sym + char );
+			console.log("cardObj w id ", cardObj);
            
       		// Create a unique reference for all cards/get their SUIT class divs
 			suitsArr = cardObj.querySelectorAll("#" + sym + char + " .suit");
@@ -142,23 +132,33 @@ window.onload= function cardMaker () {
 	       		// Get card, add class red
 	       		cardObj.classList.add("red");
 	   		} 
-
-	   		anchor.appendChild(cardObj);
-
-	   		// cardObjList.push(cardObj);
-				
-			// Add all cardObjs to anchor span, to be rendered when loop has terminated
-			cardObjList.appendChild(anchor);
+	
+		// Append all cardObjs to docFragment UL (which takes place of 1st version's anchor span tag) 
+		// document.body.appendChild(docFragment);		
 		}
 				  
 	}	
 
-	// Finally, append anchor span on to DOM after the loop has terminated 
-	// document.getElementById("card-obj-list").appendChild(anchor); 
-  // document.appendChild(cardObjList);
-
-  // cardObjList.appendChild("anchor");
-  // document.appendChild(cardObjList);
+	// Finally, append each card onto pre-existing DOM element after the loop has terminated 
+	element.appendChild(docFragment);
 	
 }();
 
+// SCRAPS
+
+// Create an anchor span on which all cards will be appended at the bottom of the loop.
+	// After the loop terminates, then append this anchor to the actual DOM. 
+	// var anchor = document.createElement("span");
+	
+
+//// DocumentFragment must be the overarching parent element of appendChilds
+	// var cardObjList = [];
+	// var i = 0;
+	// var docFragment = document.createDocumentFragment(); // ("ul");
+ 	
+ // 	while (i < 52) {
+ // 	while (i < 156) {
+ // 		cardObjList = document.createElement("ul");
+ // 		cardObjList.innerText = "List item #: " + i;
+ // 		fragment.appendChild(cardObjList);
+ // 	i++; }
