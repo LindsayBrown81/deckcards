d900210db7b3e29252199ff6dc1092f7a684f5d2
LindsayBrown81 <LindsayBrown81@users.noreply.github.com>
2017-03-07 14:16:04 -0500

    Renamed gotCard to growingCard. Renamed parentDiv to cardWrapDiv. The two main functions are not talking to each other yet, and likely need to rethink where some vars are declared to adjust scopes, e.g., make suitDiv, rankDiv global bc used in both main functions.

--- cards.js
+++ cards.js
@@ -3,9 +3,10 @@ window.onload = function cardMaker() {
 
 	"use strict";
 
-	let docFragment = document.createDocumentFragment("ul");
+	var docFragment = document.createDocumentFragment("ul");
 
-	let template = function cardTemplateMaker() {
+	
+	var template = function cardTemplateMaker() {
 	 	// Make a card object. serves as card template div that can be cloned.
 		let cardObjTemplate = {};
 	    cardObjTemplate = document.createElement("div");
@@ -13,35 +14,59 @@ window.onload = function cardMaker() {
 		docFragment.appendChild(cardObjTemplate);
 		// console.log(cardObjTemplate); // empty object at this point
 
+		// Create a reference for the card template in its growing phase/as it gets more child divs
+		const growingCard = docFragment.querySelector("#card-template");
 
-		// an array of first level child divs to add to cardObjTemplate
+		console.log(growingCard);
+
+		// Make card wrapper div but don't add text yet
+		let cardWrapDiv = document.createElement("div");
+		cardWrapDiv.className = "card flex-container"; // Good! "card flex-container"
+		// Append card wrapper div to the growing card template 
+	    growingCard.appendChild(cardWrapDiv);
+		
+		// Make card child divs but don't add text yet
+		// an array of first level child divs to add to growingCard
 		let childDivs = ["top-left", "the-center flex-container", "bottom-right upsidedown"];
 		
-		// Create a reference for the card template when populated with its child divs
-		const gotCard = docFragment.querySelector("#card-template");
-
-		console.log(gotCard);
-
-		// Make parent div but don't add text yet
-		let parentDiv = document.createElement("div");
-		parentDiv.className = "card flex-container";
-		// console.log(parentDiv); // Good! "card flex-container"
-        
-        // gotCard.insertBefore(parentDiv, gotCard.childNodes[0]);
-	    gotCard.appendChild(parentDiv, gotCard.childNodes[0]);
-		
-		
-		// Make card divs but don't add text yet
 		childDivs.forEach( function( entry ) {
 			let childDiv = document.createElement("div");
 			childDiv.className = entry;
 			// console.log(childDiv); // Good! "top-left" "bottom-right upsidedown" "the-center flex-container"
 			
-			gotCard.appendChild(childDiv);
-			// console.log("gotCard.nodeType", gotCard.nodeType); // Good! gotCard is the element node containing 3 divs.  
+			// Append the 3 child divs WITHIN the cardWrapDiv
+			growingCard.childNodes[0].appendChild(childDiv);
+			// console.log("growingCard.nodeType", growingCard.nodeType); // 1. growingCard is an element node.  
 		}); 
 
-		docFragment.appendChild(gotCard);
+		// Make 2 inner most divs but don't add text yet. MOVE THESE TWO LINES TO GLOBAL?
+		var suitDiv = document.createElement("div");
+		var rankDiv = document.createElement("div");
+		
+		suitDiv.className = "suit";
+		rankDiv.className = "rank";
+
+		// Get top-left div and append suit div and rank div to it
+		var ftl = function fillTopLeft(){
+			var getTopLeft = docFragment.querySelector(" .top-left");
+			getTopLeft.appendChild(suitDiv);
+			getTopLeft.appendChild(rankDiv);
+			console.log("getTopLeft", getTopLeft);
+			return getTopLeft;
+		}();
+
+		// Get bottom-right div, clone suit div and rank div, and append them to bottom-right div
+		var fbr = function fillBottomRight(){
+			var getBottomRight = docFragment.querySelector(" .bottom-right");
+			var suitDup = suitDiv.cloneNode();
+			var rankDup = rankDiv.cloneNode(); 
+			getBottomRight.appendChild(suitDup);
+			getBottomRight.appendChild(rankDup);
+			console.log("getBottomRight", getBottomRight);
+			return getBottomRight;
+		}();
+
+		docFragment.appendChild(growingCard);
 
 	    // Finally, append docFragment loaded w 52 cardObjs onto DOM. docFragment vanishes, appending only its sub tree.	
 		document.body.appendChild(docFragment); 
